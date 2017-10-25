8ceee0c260308173101e8299cfb4ab84bbd8642a
SkyMoan <erling_sm@hotmail.com>
2017-10-25 06:19:40 +0200

    python

--- TDT4110 - Informasjonsteknologi, Grunnkurs/Strenghåndtering.py
+++ TDT4110 - Informasjonsteknologi, Grunnkurs/Strenghåndtering.py
@@ -1,4 +1,22 @@
+string1 = 'hei'
+string2 = 'hello'
+string3 = 'hello'
 
 
+def check_equals(str1, str2):
 
-def check_equals(str1,str2)
+    return str1 is str2
+
+print(check_equals(string2, string3))
+
+def reversed_word(str):
+    reversed = ''
+    index = len(str) - 1
+
+    while index >=0:
+        reversed += str[index]
+        index-=1
+
+    return reversed
+
+print(reversed_word(string2))
